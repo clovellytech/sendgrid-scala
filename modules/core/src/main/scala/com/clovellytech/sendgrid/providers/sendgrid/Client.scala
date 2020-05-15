38f02377f88ff184444788e469aab16b80699670
Zak Patterson <pattersonzak@gmail.com>
2020-05-15 13:15:36 -0500

    Fix imports

--- modules/core/src/main/scala/com/clovellytech/sendgrid/providers/sendgrid/Client.scala
+++ modules/core/src/main/scala/com/clovellytech/sendgrid/providers/sendgrid/Client.scala
@@ -4,26 +4,16 @@ package providers.sendgrid
 import cats.effect.ConcurrentEffect
 import cats.effect.ContextShift
 import cats.effect.Resource
-import cats.effect.Sync
 import cats.effect.Timer
-import cats.effect.implicits._
 import cats.implicits._
-import cats.mtl.ApplicativeAsk
 import Codecs._
 import org.http4s.circe.CirceEntityCodec._
 import org.http4s.client._
 import org.http4s.client.blaze._
 import org.http4s.client.dsl.Http4sClientDsl
 import org.http4s.dsl.Http4sDsl
-import org.http4s.dsl.io._
-import org.http4s.Header
-import org.http4s.implicits._
-import org.http4s.Response
-import scala.concurrent.ExecutionContext
-import org.http4s.Request
 import org.http4s.Status
-import io.circe.syntax._
-import io.circe.Encoder
+import scala.concurrent.ExecutionContext
 
 class Emails[F[_]: ConcurrentEffect: ContextShift: Timer](client: Client[F], config: EmailsConfig)
     extends Http4sDsl[F]
